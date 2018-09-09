40d24e643068ba7342610566458ec9b22693930f
MoustafaHamed\Moustafa Hamed <mostafa-a.hamed@hotmail.com>
2018-09-09 22:13:10 +0200

    first commit

---
+++ BoshaProj/src/Main.java
@@ -0,0 +1,16 @@
+
+/**
+ *@author Mostafa Hamed
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		System.out.println("Hello World for Bosha's Githubs");
+
+	}
+
+}
